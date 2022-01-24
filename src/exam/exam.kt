f05331da24559415f5d529e2c60aa72e16193e89
StanislavBB <stanislav-balan03@mail.ru>
2022-01-24 12:41:59 +0300

    FFF

--- src/exam/exam.kt
+++ src/exam/exam.kt
@@ -1,6 +1,7 @@
 package exam
 
 import java.io.File
+import java.io.IOException
 
 fun example(inputName: String): Int {
     if ("ultimate question" in File(inputName).readText().toLowerCase()) {
@@ -11,6 +12,35 @@ fun example(inputName: String): Int {
 
 // Пожалуйста добавьте необходимые параметры,
 // тип результата, и измените имя функции (Shift+F6)
-fun myExamTask() {
-    TODO()
+fun correctFile(inputName: String, outputName: String) {
+    var file = File(inputName)
+    if (!file.exists()) {
+        throw IOException("file" + inputName +"not found")
+    }
+    var lines = file.readLines()
+    var writer = File(outputName).bufferedWriter()
+    var i = 0
+    while (i < lines.size) {
+        var line = lines[i]
+        if (line.contains("#")) {
+            var words = line.split(" ")
+            var newWords = mutableListOf<String>()
+            for (w in words) {
+                if (!w.startsWith("#")) {
+                    newWords.add(w)
+                }
+            }
+            line = newWords.joinToString(" ")
+        }
+        if (line.contains("$$")) {
+            val parts = line.split("$$")
+            line = parts.joinToString("\n")
+        }
+        writer.write(line)
+        if (i != lines.size - 1) {
+            writer.newLine()
+        }
+        i++
+    }
+    writer.close()
 }
--- test/exam/ExamTest.kt
+++ test/exam/ExamTest.kt
@@ -2,6 +2,9 @@ package exam
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.File
+import java.io.IOException
 
 internal class ExamTest {
 
@@ -10,8 +13,20 @@ internal class ExamTest {
         assertEquals(42, example("input/example.txt"))
     }
 
-    @Test
-    fun testMyExamTask() {
-        myExamTask()
+    private fun assertFileContent(name: String, expectedContent: String) {
+        val file = File(name)
+        val content = file.readLines().joinToString("\n")
+        assertEquals(expectedContent, content)
     }
+
+    @Test
+    fun testcorrectFile() {
+        val outFilesName = "output/correctFile.txt"
+
+        assertThrows(IOException::class.java) { correctFile("missing file", outFilesName)}
+
+        correctFile("input/correctFile.txt", outFilesName)
+        assertFileContent(outFilesName, "А роза упала на лапу Азора.\nБуратино не любит ходить в школу.")
+    }
+
 }
